7daae479c7aa9f7081911c16c26a414a7bb48149
johnng0805 <johnng0805@gmail.com>
2020-11-13 17:38:30 +0700

    Added Author's name.

--- app/src/main/java/com/example/uit_app/CourseDetailsActivity.java
+++ app/src/main/java/com/example/uit_app/CourseDetailsActivity.java
@@ -93,9 +93,12 @@ public class CourseDetailsActivity extends AppCompatActivity {
                                         JSONObject jo = ja.getJSONObject(i);
                                         JSONObject joSub = jo.getJSONObject("vote");
                                         JSONObject joCat = jo.getJSONObject("category");
+                                        JSONObject joAuthor = jo.getJSONObject("idUser");
 
                                         CourseItem item = new CourseItem();
                                         item.setTitle(jo.getString("name"));
+                                        item.setAuthor(joAuthor.getString("name"));
+                                        item.setAuthorID(joAuthor.getString("_id"));
                                         item.setTotalVote(joSub.getInt("totalVote"));
                                         item.setDiscount(jo.getInt("discount"));
                                         item.setRanking(jo.getString("ranking"));
